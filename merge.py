9d99203e87c8373eb793b339bcdcb5889c66b2dc
Carlos <ineedzpancakes@gmail.com>
2022-04-11 14:24:40 -0500

    Update merge.py

--- merge.py
+++ merge.py
@@ -1,7 +1,15 @@
+# first, have python 3 installed
+# second, install the PyPDF2 module by typing "pip install PyPDF2" in your terminal
+
+# do the following imports
 import os
 from PyPDF2 import PdfFileMerger
+
+# make sure the python file (this code) and all your pdfs are in the same folder when you run the code
 source = os.getcwd()
 
+# from here on, the merge process begins
+# if you want to merge pdfs in a particular order, that's up to you to figure it out (it's not hard)
 for item in os.listdir(source):
   if item.endswith("pdf):
     merge.append(item)
